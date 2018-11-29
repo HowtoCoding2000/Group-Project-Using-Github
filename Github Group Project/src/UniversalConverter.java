590352286ea23492358e5729a362a0754cdc7951
calvin.farrell <calvin.farrell@MU-C1-12.mullen.pvt.k12.co.us>
2018-11-29 08:30:20 -0700

    completed skeleton menu

--- Github Group Project/src/UniversalConverter.java
+++ Github Group Project/src/UniversalConverter.java
@@ -3,29 +3,44 @@ public class UniversalConverter
 	{
 		static int userConverterSelection = 0;
 		static Scanner userInput = new Scanner(System.in);
+		static Scanner userStringput = new Scanner(System.in);
+		static String userName = "";
 		public static void main(String[] args)
 			{
 				// TODO Auto-generated method stub
+				greetUser();
+				runUniversalConverter();
+			}
+		public static void greetUser()
+			{
 				System.out.println("Welcome to the Universal Converter.");
+				System.out.println("What is your name?");
+				userName = userStringput.nextLine();
+				System.out.println("Hello, " + userName + ".");
+			}
+		public static void runUniversalConverter()
+			{
 				boolean runningUniversalConverter = true;
 				while (runningUniversalConverter)
 					{
 						System.out.println("Please choose the converter you would like to use.");
 						System.out.println("(1) Temperature Converter.");
-						System.out.println("(2) Calculator");
-						System.out.println("(3) Weight Converter");
+						System.out.println("(2) Currency Converter");
+//						System.out.println("(3) Weight Converter");
 						userConverterSelection = userInput.nextInt();
 						//switch statement directing toward converter methods
 						switch (userConverterSelection)
 						{
 							case 1:
 									{
+										runningUniversalConverter = false;
 										TemperatureConverter.convertTemperature();
 									}
-//							case 2:
-//									{
-//										converter method
-//									}
+							case 2:
+									{
+										runningUniversalConverter = false;
+										CurrencyConvertor.currency();
+									}
 //							case 3:
 //									{
 //										converter method
